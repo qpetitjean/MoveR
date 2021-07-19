afb912e75e9e4b0ff3fd471be32889eb8e70869f
Quentin <q.petitjean1@gmail.com>
2021-07-19 09:53:51 +0200

    Update sinuosity.R

--- R/sinuosity.R
+++ R/sinuosity.R
@@ -1,22 +1,22 @@
-#' @title Compute corrected rediscretized sinuosity index 
+#' @title Compute corrected rediscretized sinuosity index
 #'
 #'
-#' @description Given a data frames containing tracking informations for a given fragment, 
-#' this function rediscretize fragment path and returns a vector containing the value of 
+#' @description Given a data frames containing tracking informations for a given fragment,
+#' this function rediscretize fragment path and returns a vector containing the value of
 #' sinuosity along this fragment
-#' 
+#'
 #'
 #' @param df A data frame containing x, y coordinates and time columns named "x.pos", "y.pos", "frame" for a fragment
-#' 
-#' @param scale A ratio corresponding to the scaling factor to be applied to the trajectory coordinates 
+#'
+#' @param scale A ratio corresponding to the scaling factor to be applied to the trajectory coordinates
 #' (e.g., size in cm / size in pixels; see trajr::TrajScale())
-#' 
+#'
 #' @param unit The unit expected after scaling (e.g., "cm", "m", ...)
-#' 
-#' @param segL A numeric value expressed in the unit specified by user and corresponding 
-#' to the length of the resampling step needed to discretize the input trajectory 
-#' see trajr::TrajRediscretize())
-#' 
+#'
+#' @param segL A numeric value expressed in the unit specified by user and corresponding
+#' to the length of the resampling step needed to discretize the input trajectory (optional)
+#' see trajr::TrajRediscretize()) 
+#'
 #'
 #' @return this function returns a vector containing the value of sinuosity for a given fragment
 #'
@@ -31,27 +31,37 @@
 #'
 #' @export
 
-sinuosity <- function(df, scale = NULL, unit = NULL, segL) {
-  if(is.null(unit)) { 
+sinuosity <- function(df,
+                      scale = NULL,
+                      unit = NULL,
+                      segL = NULL) {
+  if (is.null(unit)) {
     warning("the unit of the trajectory path after scaling is missing, default is pixels")
     unit = "pixels"
   }
-  if(is.null(scale)) { 
-    ("the scaling factor to be applied to the trajectory coordinates is missing, default is 1/1")
-    scale = 1/1
+  if (is.null(scale)) {
+    (
+      "the scaling factor to be applied to the trajectory coordinates is missing, default is 1/1"
+    )
+    scale = 1 / 1
   }
   trj <-
     trajr::TrajFromCoords(df[, c("x.pos", "y.pos", "frame")],
                           spatialUnits = "pixels",
                           timeCol = 3)
   trj <- trajr::TrajScale(trj, scale, unit)
-  trjL <- trajr::TrajLength(trj)
-  if (trjL >= segL) {
-    discretized <- trajr::TrajRediscretize(trj, segL)
-    sinuosityRes <- trajr::TrajSinuosity2(discretized)
-  } else if (trjL < segL) {
-    sinuosityRes <- NA
-    warning("The fragment is shorter than segL, sinuosity returned NA, segL might be to high")
+  if (is.null(segL)) {
+    sinuosityRes <- trajr::TrajSinuosity2(trj)
+  } else if (!is.null(segL)) {
+    if (inherits(try(trajr::TrajRediscretize(trj, segL), silent = TRUE)
+                 , "try-error")) {
+      sinuosityRes <- NA
+      warning("The fragment is shorter than segL, sinuosity returned NA, segL might be to high")
+    } else if (!inherits(try(trajr::TrajRediscretize(trj, segL), silent = TRUE)
+                         , "try-error")) {
+      discretized <- trajr::TrajRediscretize(trj, segL)
+      sinuosityRes <- trajr::TrajSinuosity2(trj)
+    }
   }
   return(sinuosityRes)
 }
