a0ebad49a74824f03d6cf604237842a4be0c7672
Quentin <q.petitjean1@gmail.com>
2021-10-07 20:37:55 +0200

    Update analyseFrags.R

--- R/analyseFrags.R
+++ R/analyseFrags.R
@@ -1,17 +1,14 @@
 #' @title Fragments analyses
 #'
 #'
-#' @description Given a list of data frames containing tracking informations for each fragment and 
-#' a custom function, this function perform the computation and returns the original list 
-#' of data frames with the result of the analysis appended 
-#' 
+#' @description Given a list of data frames containing tracking informations for each fragment and
+#' a custom function, this function perform the computation and returns the original list
+#' of data frames with the result of the analysis appended
+#'
 #'
 #' @param trackDat A list of data frame containing tracking informations for each fragment
-#' 
-#' @param customFunc A function used to perform the computation along all fragments
-#' 
-#' @param VarName A character string specifying the name of the computed metric (optional) by default the function
-#' retrieve the name of the custom function and consider it as the metric name
+#'
+#' @param customFunc A function or a list of functions used to perform the computation along all fragments
 #'
 #' @return this function returns a new list of dataframe corresponding to each new fragments with the result of customFunc appended
 #'
@@ -26,38 +23,58 @@
 #'
 #' @export
 
-analyseFrags <- function(trackDat, customFunc, VarName = NULL) {
-  
-  # in case VarName is not specified, extract the name of the custom function
-  if (is.null(VarName)) {
-    VarName <- strsplit(sub("\\(.*", "", deparse(customFunc)), " ")
-    if (length(VarName) > 1) {
-      VarName = VarName[[2]]
-    }
+analyseFrags <- function(trackDat, customFunc) {
+  if (is.null(customFunc)) {
+    stop("customFunc argument is missing, a customFunc is needed to compute metric")
   }
-  # loop trough the fragment list and append the result to each list by using the name of the custom function   
+  # if customFunc is a unnamed list of function, retrieve function names
+  if (is.list(customFunc)) {
+    if (is.null(names(customFunc))) {
+      VarName <-
+        lapply(customFunc, function(x)
+          strsplit(sub("\\(.*", "", deparse(x)), " ")[[2]])
+      names(customFunc) <- unlist(VarName)
+    }
+    # if customFunc is a function retrieve function names and transformed it to a named list
+  } else if (is.function(customFunc)) {
+    VarName <- strsplit(sub("\\(.*", "", deparse(customFunc)), " ")[[2]]
+    customFunc <- list(customFunc)
+    names(customFunc) <- VarName
+  }
+  
+  # loop trough the fragment list and append the result to each list by using the name of the custom function
   total = length(trackDat)
   pb <-
     progress::progress_bar$new(format = "fragments processing [:bar] :current/:total (:percent)", total = total)
   pb$tick(0)
   Sys.sleep(0.001)
-  
-  if(is.null(names(trackDat))){ 
+  # if trackDat is an unnamed list of fragment use the position of the fragment in the list as fragment name
+  if (is.null(names(trackDat))) {
     names(trackDat) <- seq(length(trackDat))
-  } 
-    
-  for(i in names(trackDat)){
-    if (!inherits(try(customFunc(trackDat[[i]]), silent = T)
-                  , "try-error") & !class(customFunc(trackDat[[i]])) == "function") {
-    trackDat[[i]][[VarName]] <- customFunc(trackDat[[i]])
-    } else if (inherits(try(customFunc(trackDat[[i]]), silent = T)
-                        , "try-error") | class(customFunc(trackDat[[i]])) == "function") {
-      trackDat[[i]][[VarName]] <- NA
-      warning("In fragment ",i , " : analyseFrags returned NA, perhaps check customFunc parameters")
+  }
+  for (i in names(trackDat)) {
+    for (j in names(customFunc)) {
+      if (!inherits(try(customFunc[[j]](trackDat[[i]]), silent = T)
+                    , "try-error") &
+          !class(customFunc[[j]](trackDat[[i]])) == "function") {
+        trackDat[[i]][[j]] <- customFunc[[j]](trackDat[[i]])
+      } else if (inherits(try(customFunc[[j]](trackDat[[i]]), silent = T)
+                          , "try-error") |
+                 class(customFunc[[j]](trackDat[[i]])) == "function") {
+        trackDat[[i]][[j]] <- NA
+        warning(
+          "For customFunc ",
+          j ,
+          " in fragment ",
+          i ,
+          " : \nanalyseFrags returned NA, perhaps check customFunc argument"
+        )
+      }
     }
     # progress bar
     pb$tick(1)
     Sys.sleep(1 / 1000)
   }
+  
   return(trackDat)
 }
