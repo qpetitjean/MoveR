33fca690eed40fb642246d05f1b79a2a0ca0a95f
Quentin <q.petitjean1@gmail.com>
2021-10-20 18:09:37 +0200

    Update analyseTime.R

--- R/analyseTime.R
+++ R/analyseTime.R
@@ -13,7 +13,7 @@
 #' @param customFunc A function used to perform the computation across time
 #'
 #' @param Tinterval A vector containing two numeric values expressed in the timeline unit and
-#' specifying the time interval on which the computation is performed 
+#' specifying the time interval on which the computation is performed
 #' (default is null, meaning the computation will be performed on the whole timeline)
 #'
 #' @param Tstep A numeric value expressed in the timeline unit and specifying the size of the
@@ -45,10 +45,10 @@ analyseTime <-
            customFunc = NULL,
            Tinterval = NULL,
            Tstep = 1,
-           sampling = 1, 
+           sampling = 1,
            wtd = FALSE) {
-  
-    if (is.null(timeCol) | is.null(timeCol %in% unlist(lapply(trackDat, names)))) {
+    if (is.null(timeCol) |
+        is.null(timeCol %in% unlist(lapply(trackDat, names)))) {
       stop(
         "timeCol argument is missing or is not found in the provided dataset, timeCol might be misspelled"
       )
@@ -56,40 +56,6 @@ analyseTime <-
     if (is.null(customFunc)) {
       stop("customFunc argument is missing, a customFunc is needed to compute metric")
     }
-    
-    if (is.null(Tinterval)) {
-      # define the timeline
-      timeline <- seq(min(unlist(lapply(trackDat, function (w)
-        min(w[timeCol], na.rm = T))), na.rm = T),
-        max(unlist(lapply(trackDat, function (w)
-          max(w[timeCol], na.rm = T))), na.rm = T), by = 1)
-      # compute sliding mean every n time unit (according to "sampling" parameter) allow to make computation faster
-      Newtimeline <- seq(from = timeline[1],
-                         to = timeline[length(timeline)],
-                         by = sampling)
-      Newtimeline[length(Newtimeline)+1] <- timeline[length(timeline)]
-      Newtimeline[which(Newtimeline == 0)] <- 1
-    } else {
-      # define the timeline
-      timeline <- seq(from = round(Tinterval[1] - ((Tstep - 1) / 2)),
-                      to = round(Tinterval[2] + ((Tstep - 1) / 2)),
-                      by = 1)
-      # compute sliding mean every n time unit (according to "sampling" parameter) allow to make computation faster
-      Newtimeline <- seq(from = Tinterval[1],
-                         to = Tinterval[2],
-                         by = sampling)
-      Newtimeline[length(Newtimeline)+1] <- Tinterval[2]
-      Newtimeline[which(Newtimeline == 0)] <- 1
-    }
-    
-    # initialize result vector
-    smoothed <- vector()
-    # initialize progress bar
-    total = length(Newtimeline)
-    pb <-
-      progress::progress_bar$new(format = "frame processing [:bar] :current/:total (:percent)", total = total)
-    pb$tick(0)
-    
     # if customFunc is a unnamed list of function, retrieve function names
     if (is.list(customFunc)) {
       if (is.null(names(customFunc))) {
@@ -104,6 +70,62 @@ analyseTime <-
       customFunc <- list(customFunc)
       names(customFunc) <- VarName
     }
+    if (is.null(Tinterval)) {
+      # define the timeline
+      timeline <- seq(min(unlist(lapply(trackDat, function (w)
+        min(w[timeCol], na.rm = T))), na.rm = T),
+        max(unlist(lapply(trackDat, function (w)
+          max(w[timeCol], na.rm = T))), na.rm = T), by = 1)
+      # compute sliding mean every n time unit (according to "sampling" parameter) allow to make computation faster
+      Newtimeline <- seq(from = timeline[1],
+                         to = timeline[length(timeline)],
+                         by = sampling)
+      Newtimeline[length(Newtimeline) + 1] <-
+        timeline[length(timeline)]
+      Newtimeline[which(Newtimeline == 0)] <- 1
+    } else {
+      # define the timeline
+      timeline <-
+        seq(
+          from = round(Tinterval[1] - ((Tstep - 1) / 2)),
+          to = round(Tinterval[2] + ((Tstep - 1) / 2)),
+          by = 1
+        )
+      # compute sliding mean every n time unit (according to "sampling" parameter) allow to make computation faster
+      Newtimeline <- seq(from = Tinterval[1],
+                         to = Tinterval[2],
+                         by = sampling)
+      Newtimeline[length(Newtimeline) + 1] <- Tinterval[2]
+      Newtimeline[which(Newtimeline == 0)] <- 1
+    }
+    # if customFunc is a unnamed list of function, retrieve function names
+    if (is.list(customFunc)) {
+      if (is.null(names(customFunc))) {
+        VarName <-
+          lapply(customFunc, function(x)
+            strsplit(sub("\\(.*", "", deparse(x)), " ")[[2]])
+        names(customFunc) <- unlist(VarName)
+      }
+      # if customFunc is a function retrieve function names and transformed it to a named list
+    } else if (is.function(customFunc)) {
+      VarName <- strsplit(sub("\\(.*", "", deparse(customFunc)), " ")[[2]]
+      customFunc <- list(customFunc)
+      names(customFunc) <- VarName
+    }
+    # initialize result list
+    smoothed <- list()
+    for (name in names(customFunc)) {
+      temp_df <-
+        data.frame(matrix(NA, nrow = length(Newtimeline), ncol = 2))
+      colnames(temp_df) <- c(name, timeCol)
+      smoothed[[name]] <- temp_df
+    }
+    # initialize progress bar
+    total = length(Newtimeline)
+    pb <-
+      progress::progress_bar$new(format = "frame processing [:bar] :current/:total (:percent)", total = total)
+    pb$tick(0)
+    
     # loop trough fragments part to compute metrics according to customFunc
     for (i in Newtimeline) {
       # Select Time interval according to the specified Tstep and extract the concerned fragments part
@@ -128,45 +150,55 @@ analyseTime <-
           ) / 2)))):which(timeline == round((i + ((
             Tstep - 1
           ) / 2))))]
-        # select the fragment that are detected in the selected timeline part and 
-        # Cut them according the selected part of the timeline 
+        # select the fragment that are detected in the selected timeline part and
+        # Cut them according the selected part of the timeline
         WhoWhen <- cutFrags(trackDat, function (x)
           x[[timeCol]] %in% selVal)
         # compute the metrics specified through customFunc on the selected fragment part
-        # initialize result vector for a given Time interval
-        Res <- vector()
-        len <- vector()
-        # compute the metrics specified through customFunc on the selected fragment part
         # initialize result list for a given Time interval
         Res <- list()
-        len <- vector()
+        len <- list()
         # loop trough fragments on a given Time interval
         for (j in names(WhoWhen)) {
           df <- WhoWhen[[j]]
           for (n in seq(length(customFunc))) {
+            #store the result of each computation in a list
             Res_temp <- customFunc[[n]](df)
             Res[[names(customFunc)[n]]] <-
               c(Res[[names(customFunc)[n]]], Res_temp)
+            #store the length of each fragment used in the computation in a list
+            len_temp <- length(df[[timeCol]])
+            len[[names(customFunc)[n]]] <-
+              c(len[[names(customFunc)[n]]], len_temp)
           }
-          len_temp <- length(df[[timeCol]])
-          len <- c(len, len_temp)
         }
-        
         # compute the metric mean on a given Time interval (across fragments)
         ## in case weighed argument is FALSE, compute a simple mean
-        if(wtd == FALSE){ 
-          smoothed_temp <- mean(Res, na.rm = T)
-        } else if (wtd == TRUE) { 
+        if (wtd == FALSE) {
+          smoothed_temp <- lapply(Res, function(x)
+            mean(x, na.rm = T))
+        } else if (wtd == TRUE) {
           ## in case weighed argument is TRUE, compute a weighed mean according to fragment length (Timecol)
-          # create an equivalent of na.rm = T and compute the weighed mean
-          na.rm <- !is.na(Res) & !is.na(len)
-          Res <- Res[na.rm]
-          len <- len[na.rm]
-          smoothed_temp <- sum(len * Res) / sum(len) 
-            }
-        smoothed <- c(smoothed, smoothed_temp)
+          # create an equivalent of na.rm = T and compute the weighed mean for each metric
+          Reslen <-
+            lapply(names(Res), function(x)
+              data.frame(Res = Res[[x]], len = len[[x]]))
+          names(Reslen) <- names(Res)
+          Reslen <- lapply(Reslen, function(x)
+            na.omit(x))
+          smoothed_temp <-
+            lapply(Reslen, function(x)
+              sum(x$len * x$Res) / sum(x$len))
+        }
+        # append the results in the smoothed list
+        for (n in names(smoothed)) {
+          smoothed[[n]][which(Newtimeline == i), ] <- c(smoothed_temp[[n]], i)
+        }
       } else {
-        smoothed <- c(smoothed, NA)
+        # append the results in the smoothed list
+        for (n in names(smoothed)) {
+          smoothed[[n]][which(Newtimeline == i), ] <- c(NA, i)
+        }
       }
       # progress bar
       pb$tick(1)
