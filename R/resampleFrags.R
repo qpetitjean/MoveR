dd2c3a5ffa853125e889ab4c909474fcc67e20a9
Quentin <q.petitjean1@gmail.com>
2022-03-10 13:01:09 +0100

    Update resampleFrags.R

--- R/resampleFrags.R
+++ R/resampleFrags.R
@@ -8,7 +8,9 @@
 #' @param trackDat A list of data frame containing tracking informations for each fragment
 #' (e.g., maj.ax, angle, min.ax, x.pos, y.pos, ...)
 #'
-#' @param Tstep A numeric value corresponding to the length of the resampling step (number of frames)
+#' @param Tstep A numeric value corresponding to the length of the resampling step (unit of time, e.g., frame)
+#' 
+#' @param TimeCol A character string corresponding to the name of the column containing Time information (e.g., "frame")
 #' 
 #' @return A list of data frame containing resampled tracking informations for each fragment 
 #'
@@ -21,13 +23,16 @@
 #' 
 
 
-resampleFrags <- function(trackDat, Tstep = NULL) {
+resampleFrags <- function(trackDat, Tstep = NULL, TimeCol = NULL) {
 
   if (is.null(Tstep)) {
     stop(
       "Tstep argument is missing: a value corresponding to the resampling step is needed to resample the data"
     )}
-  
+  if (is.null(TimeCol)) {
+    stop(
+      "TimeCol argument is missing: the name of the column carying time information is needed to resample the data"
+    )}
 # initialize progress bar
 total = length(trackDat)
 pb <-
@@ -37,22 +42,22 @@ Sys.sleep(0.001)
 
 for(i in seq(length(trackDat))){ 
 # resampling fragment according to Tstep
-# identify the first frame where the fragment is detected
-start <- trackDat[[i]][trackDat[[i]]$frame == sort(trackDat[[i]]$frame),][1,]
+# identify the first time unit where the fragment is detected
+start <- trackDat[[i]][trackDat[[i]][[TimeCol]] == sort(trackDat[[i]][[TimeCol]]),][1,]
 # use it as the starting point of the increment for resempling
-increment <- seq(from = start$frame, to = max(trackDat[[i]]$frame, na.rm = T), by = Tstep)
-selFrames <- increment %in% trackDat[[i]]$frame 
-# store frame to keep and detect which have to be added
+increment <- seq(from = start[[TimeCol]], to = max(trackDat[[i]][[TimeCol]], na.rm = T), by = Tstep)
+selFrames <- increment %in% trackDat[[i]][[TimeCol]]
+# store the time values to keep and detect which have to be added
 toKeep_temp <- increment[selFrames]
-# retrieve data of the fragment and add a row containing NA when the frame is missing
-toKeep <- trackDat[[i]][trackDat[[i]]$frame %in% toKeep_temp, ]
+# retrieve data of the fragment and add a row containing NA when a time value is missing
+toKeep <- trackDat[[i]][trackDat[[i]][[TimeCol]] %in% toKeep_temp, ]
 if (FALSE %in% selFrames) {
   toAdd <-
     setNames(data.frame(matrix(
-      ncol = dim(trackDat[[i]])[[2]], nrow = length(list(increment[which(selFrames == FALSE)]))
+      ncol = ncol(trackDat[[i]]), nrow = length(list(increment[which(selFrames == FALSE)]))
     )), c(names(trackDat[[i]])))
-  toAdd$frame <- c(unlist(list(increment[which(selFrames == FALSE)])))
-  toKeep <- rbind(toKeep, toAdd)[order(rbind(toKeep, toAdd)$frame),]
+  toAdd[TimeCol] <- c(unlist(list(increment[which(selFrames == FALSE)])))
+  toKeep <- rbind(toKeep, toAdd)[order(rbind(toKeep, toAdd)[[TimeCol]]),]
   if ("frags_id" %in% names(trackDat[[i]]) == TRUE) {
     w1 <- paste("In fragment", unique(trackDat[[i]]$frags_id), sep = " ")
     w2 <-
